7b02077d54c8245732447af9dcd012527f167523
Pintopie <hoangnhan20192@gmail.com>
2024-07-11 23:27:29 +0700

    Initial commit 	new file:   .cpanel.yml 	new file:   COPYRIGHT/assets/images/dev/LICENSE 	new file:   COPYRIGHT/assets/images/dev/LICENSE-PEXELS.txt 	new file:   COPYRIGHT/copyright 	new file:   assets/config.js 	new file:   assets/core.css 	new file:   assets/cpanel.css 	new file:   assets/css/materialdesignicons.css 	new file:   assets/css/materialdesignicons.css.map 	new file:   assets/css/materialdesignicons.min.css 	new file:   assets/css/materialdesignicons.min.css.map 	new file:   assets/fonts/materialdesignicons-webfont.eot 	new file:   assets/fonts/materialdesignicons-webfont.svg 	new file:   assets/fonts/materialdesignicons-webfont.ttf 	new file:   assets/fonts/materialdesignicons-webfont.woff 	new file:   assets/fonts/materialdesignicons-webfont.woff2 	new file:   assets/images/apple-touch-icon.png 	new file:   assets/images/dev/LICENSE 	new file:   assets/images/dev/LICENSE-PEXELS.txt 	new file:   assets/images/dev/Portrait.jpg 	new file:   assets/images/dev/background.jpg 	new file:   assets/images/favicon.ico 	new file:   dist/chunk-0.js 	new file:   dist/dll.vendor.js 	new file:   dist/main.js 	new file:   index.html

---
+++ assets/config.js
@@ -0,0 +1,45 @@
+window.cpanel = {
+    data: {
+        phone: "+84 939 813 965",
+        fax: "",
+        email: "hoangnhan20192@gmail.com",
+        address: "Ho Chi Minh City",
+        social: [
+            
+            {
+                icon: 'facebook',
+                link: "https:\/\/www.facebook.com\/nghnhan16903\/"
+            },
+            
+            
+            
+            
+            
+            {
+                icon: 'linkedin',
+                link: "https:\/\/www.linkedin.com\/in\/kenny-nguyen-42ba2624b\/"
+            },
+            
+            
+        ]
+    },
+    style: {
+        primary: "#a8e6c0",
+        accent: "#7ecef7"
+    },
+    slides: [
+        {
+            prefix: "Hi! This is",
+            title: "Kenny Nguyen",
+            subtitle: "Computer Science Student",
+            type: 'vcard',
+            backgroundImage: "",
+            backgroundColor: "#ffffff",
+            color: "",
+            buttonText: "",
+            buttonLink: "",
+            biography: "Hi, I’m @Kenny Nguyen, Computer Science Student.\n👀 I’m interested in web APIS, A.I engineering, software developing and cloud services\n🌱 I’m currently doing college coursework in Software Development at Broward College.\nI’m looking to collaborate on open-source projects",
+            portraitImage: ""
+        }
+    ]
+};
